e7240085b251abd8683afb11cdb735f656586acc
zhanghaitao <1209782739@qq.com>
2020-08-29 18:02:38 +0800

    inti

--- datasort.py
+++ datasort.py
@@ -1,11 +1,6 @@
-import pandas as pd
 import csv
 import requests
 import json
-import math
-import datetime
-import time as t
-from datetime import datetime
 
 input_file = open(r'D:\final.csv', 'rt', encoding='utf8')
 csv_file = csv.reader(input_file)
@@ -46,49 +41,51 @@ def timeSort(dataList):
     return list0_6, list6_9, list9_12, list12_15, list15_18, list18_21, list21_24
 
 def transform(data1,data2):
-    url = 'http://api.map.baidu.com/geoconv/v1/?coords={},{}&from=1&to=5&ak=vKHOVd6Gd0ZECdBgwuOcSCaTvX67bD3G'.format(data1,data2)
+    url = 'http://api.map.baidu.com/geoconv/v1/?coords={},{}&from=1&to=5&ak=0FuoX30MFf7YMrdS5Wi9GGAcHBblKDuu'.format(data1, data2)
     req = requests.get(url)
     text = req.text
     text = json.loads(text)
+    print(text)
     x = text['result'][0]['x']
     y = text['result'][0]['y']
-    print(x)
     return x, y
 def traffic(list):
-    list1 = [] #label=1
-    list2 = [] #label=2
-    list3 = [] #label=3
-    list4 = [] #label=4
+    list1 = [] #label=1 步行
+    list2 = [] #label=2 公交车
+    list3 = [] #label=3 小汽车
+    list4 = [] #label=4 地铁
 
     for data in list:
         listx = []
-        if data[9] == '1':
-            x, y = transform(float(data[3]), float(data[4]))
-            listx.append(y)
-            listx.append(x)
-            list1.append(listx)
-        elif data[9] == '2':
-            x, y = transform(float(data[3]), float(data[4]))
-            listx.append(y)
-            listx.append(x)
-            list2.append(listx)
-        elif data[9] == '3':
-            x, y = transform(float(data[3]), float(data[4]))
-            listx.append(y)
-            listx.append(x)
-            list3.append(listx)
-        else:
+        # if data[9] == '1':
+        #     x, y = transform(float(data[3]), float(data[4]))
+        #     listx.append(y)
+        #     listx.append(x)
+        #     list1.append(listx)
+        # if data[9] == '2':
+        #     x, y = transform(float(data[3]), float(data[4]))
+        #     listx.append(y)
+        #     listx.append(x)
+        #     list2.append(listx)
+        # if data[9] == '3':
+        #     x, y = transform(float(data[3]), float(data[4]))
+        #     listx.append(y)
+        #     listx.append(x)
+        #     list3.append(listx)
+        if data[9] == '4':
             x, y = transform(float(data[3]), float(data[4]))
             listx.append(y)
             listx.append(x)
             list4.append(listx)
     print(list1)
+    print("***********************************")
     print(list2)
+    print("***********************************")
     print(list3)
+    print("***********************************")
     print(list4)
 
 if __name__ == '__main__':
     dataList = readData(csv_file)
     list0_6, list6_9, list9_12, list12_15, list15_18, list18_21, list21_24 = timeSort(dataList)
-    traffic(list0_6)
-
+    traffic(list21_24)
